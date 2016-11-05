861bcc2aa237d0becda0cbaa872763aacdfb45eb
rihbyne <rihen234@gmail.com>
2016-11-05 12:31:42 +0530

    secure http

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://rihbyne.github.io'
+SITEURL = 'https://rihbyne.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
