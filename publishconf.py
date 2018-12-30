76c7cf386a6be424c05a2dcbc05786d59a8f64f6
Rihan <rihen234@gmail.com>
2018-12-31 01:51:33 +0530

    added ssl to http site

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # SITEURL = 'http://swlogs.me'
-SITEURL = 'http://rihbyne.github.io'
+SITEURL = 'https://rihbyne.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
